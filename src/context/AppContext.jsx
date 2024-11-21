15389c24007f9fdeeaf3f726249863b9d4c64bd4
KelsGaming <alcorizamichael@gmail.com>
2024-11-22 06:54:59 +0800

    Add Login and Register functionality

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -9,7 +9,7 @@ const AppContextProvider = (props) => {
     const currencySymbol = 'Php'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [doctors, setDoctors] = useState([])
-    const [token, setToken] = useState('')
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
 
     
 
