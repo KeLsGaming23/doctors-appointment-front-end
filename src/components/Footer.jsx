4fb6ac4b1987f06d676b84edac06d754ae5fb261
KelsGaming <alcorizamichael@gmail.com>
2024-12-30 14:59:20 +0800

    Boss Dex edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,11 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             {/* -left side- */}
             <div>
-                <img className='mb-5 w-40' src={assets.logo} alt="" />
-                <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                {/*<img className='mb-5 w-40' src={assets.logo} alt="" /> */}
+                <p className='text-xl font-medium mb-5'>DOCTORS SCHEDULE</p>
+                <p className='text-md w-full md:w-2/3 text-gray-600 leading-6'>Central Luzon Doctors Hospital Gerona Tarlac <br />Clinic room 3 <br />Monday, Tuesday Thursday and Friday <br />12 noon - 2 pm </p>
+                <p className='text-md  mt-2 w-full md:w-2/3 text-gray-600 leading-6'>Central Luzon Doctors Hospital Gerona Tarlac <br />Clinic room 3 <br />Monday, Tuesday Thursday and Friday <br />12 noon - 2 pm </p>
+                <p className='text-md  mt-2 w-full md:w-2/3 text-gray-600 leading-6'>Central Luzon Doctors Hospital Gerona Tarlac <br />Clinic room 3 <br />Monday, Tuesday Thursday and Friday <br />12 noon - 2 pm </p>
             </div>
             {/* -Center side- */}
             <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,8 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-        <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt='' />
+        <p className='text-3xl font-semibold'>Tarlac Medical Center</p>
+        {/* <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt='' /> */}
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to='/'>
                 <li className='py-1'>HOME</li>
